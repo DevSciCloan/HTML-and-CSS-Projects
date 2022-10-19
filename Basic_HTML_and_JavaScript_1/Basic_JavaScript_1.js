7b4611e9f04f56c4a386b3d20de041dd427df609
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2022-10-19 15:35:02 -0600

    Basic HTML and JavaScript 1
    Added three new files as per instruction Basic_HTML_1.html, Basic_HTML_2.html, Basic_JavaScript_1.js

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+function My_First_Function() {
+    var result = document.getElementById("Irish");
+    var String = "Kiss me, I'm Irish!";
+    result.innerHTML = String;
+    result.style.color = "green";
+    result.style.background = "black";
+}
