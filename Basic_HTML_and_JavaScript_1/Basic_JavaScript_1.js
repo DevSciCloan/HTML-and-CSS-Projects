925dfa5da9b2b67aa7a5ab45a217bbac1ee984a1
DevSciCloan <116118420+DevSciCloan@users.noreply.github.com>
2022-10-20 12:56:41 -0600

    Rewrote My_First_Function()
    Changed the way var String text is assigned color from using style.color to fontcolor()

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -1,7 +1,8 @@
 function My_First_Function() {
     var result = document.getElementById("Irish");
     var String = "Kiss me, I'm Irish!";
+    String = String.fontcolor("green"); // Works even though fontcolor() is deprecated
     result.innerHTML = String;
-    result.style.color = "green";
+    //result.style.color = "green";
     result.style.background = "black";
 }
